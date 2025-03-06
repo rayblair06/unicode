a75a75abe72e15847728bdecbd93b8c4ffe878cb
Ray Blair <rayblair06@hotmail.com>
2025-03-06 20:25:44 +0000

    [Feat] More Unicode characters

--- tests/UnicodeTest.php
+++ tests/UnicodeTest.php
@@ -2,16 +2,34 @@
 
 namespace Tests;
 
-use Rayblair\Unicode\Unicode;
 use PHPUnit\Framework\TestCase;
+use Rayblair\Unicode\Unicode;
 
 class UnicodeTest extends TestCase
 {
     public function testUnicodeGetAllFunction()
     {
-        $expectedCount = 126;
+        $expectedCount = 154998;
 
         $assertedCount = count(Unicode::getAll());
+
+        $this->assertEquals($expectedCount, $assertedCount);
+    }
+
+    public function testUnicodeGetFunction()
+    {
+        $expectedCount = 100;
+
+        $assertedCount = count(Unicode::get(100));
+
+        $this->assertEquals($expectedCount, $assertedCount);
+    }
+
+    public function testUnicodeRangeHexFunction()
+    {
+        $expectedCount = 26;
+
+        $assertedCount = count(Unicode::range('0041', '005A'));
 
         $this->assertEquals($expectedCount, $assertedCount);
     }
@@ -19,17 +37,17 @@ class UnicodeTest extends TestCase
     public function testUnicodeGetSingleFunction()
     {
         $expectedUnicode = '←';
-        
-        $assertedUnicode = Unicode::LEFT_ARROW;
-        
+
+        $assertedUnicode = Unicode::LEFTWARDS_ARROW;
+
         $this->assertEquals($expectedUnicode, $assertedUnicode);
     }
-    
+
     public function testUnicodeGetEncodeSingleFunction()
     {
         $expectedUnicode = '\u2190';
-        
-        $assertedUnicode = Unicode::escape(Unicode::LEFT_ARROW);
+
+        $assertedUnicode = Unicode::escape(Unicode::LEFTWARDS_ARROW);
 
         $this->assertEquals($expectedUnicode, $assertedUnicode);
     }
